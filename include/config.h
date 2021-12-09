a39556a4e742c751359af832f022ab3cfb6fb511
vingocbk <vi.ngoc.bk@gmail.com>
2021-12-09 17:00:47 +0700

    update form value

---
+++ include/config.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define RESPONSE_LENGTH 2048     //do dai data nhan ve tu tablet
+#define EEPROM_BLUE_SSID_START 0
+#define EEPROM_BLUE_SSID_END 32
+#define EEPROM_BLUE_PASS_START 33
+#define EEPROM_BLUE_PASS_END 64
+#define EEPROM_BLUE_DEVICE_ID_START 65
+#define EEPROM_BLUE_DEVICE_ID_END 66
+#define EEPROM_BLUE_SERVER_START 67
+#define EEPROM_BLUE_SERVER_END 128
+#define EEPROM_BLUE_MAX_CLEAR 512
+
+#define MSG_MASTER_ID         0
+#define MSG_GET_STATUS        1
+#define MSG_CONTROL_OPEN      2
+#define MSG_CONTROL_CLOSE     3
+#define MSG_CONTROL_STOP      4
+#define MSG_CONTROL_LED_VOICE 5
+#define MSG_CONTROL_LED_HAND  6
+#define MSG_RESET_DISTANT     7
+#define MSG_TIME_RETURN       8
+#define MSG_MODE_RUN          9
+#define MSG_PERCENT_LOW       10
+#define MSG_DELAY_ANALOG      11
+#define MSG_ERROR_ANALOG      12
+#define MSG_AUTO_CLOSE        13
+#define MSG_MIN_STOP_SPEED    14
+
+#define MSG_SET_ID       100
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <ESP32CAN.h>
 #include <CAN_config.h>
 #include "AppDebug.h"
+#include "config.h"
 #include "soc/soc.h"  //Brownout detector was triggered
 #include "soc/rtc_cntl_reg.h"
 CAN_device_t CAN_cfg;               // CAN Config
@@ -14,17 +15,86 @@ BluetoothSerial SerialBT;
 
 
 void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+	switch (event)
+	{
+	case ESP_SPP_SRV_OPEN_EVT:
+		ECHOLN("Client Connected");
+		break;
+	case ESP_SPP_CLOSE_EVT:
+		ECHOLN("Client Disconnected");
+		break;
+	case ESP_SPP_DATA_IND_EVT:
+		if (param->data_ind.len < 512) {
+			String data;
+			for(int i = 0; i < param->data_ind.len; i++){
+				data += (char)param->data_ind.data[i];
+			}
+			ECHO("data: ");
+			ECHOLN(data);
+			DynamicJsonDocument doc(1024);
+			deserializeJson(doc, data);
+			JsonObject obj = doc.as<JsonObject>();
+			// String type = obj["sensor"].isNull();
+			if(obj["type"].isNull())
+			{
+				String type = obj["type"].as<String>();
+				if(type.equals("get_name"))
+				{
 
-    if(event == ESP_SPP_DATA_IND_EVT){
-        if (param->data_ind.len < 512) {
-            String data;
-            for(int i = 0; i < param->data_ind.len; i++){
-                data += (char)param->data_ind.data[i];
-            }
-            ECHO("data: ");
-            ECHOLN(data);
+				}
+				else if(type.equals("change_rgb"))
+				{
+
+				}
+				else if(type.equals("on_off_led"))
+				{
+
+				}
+				else if(type.equals("change_alpha"))
+				{
+
+				}
+				else if(type.equals("control"))
+				{
+					String data = obj["data"].as<String>();
+					if(data.equals("open"))
+					{
+						CAN_frame_t tx_frame;
+						tx_frame.FIR.B.FF = CAN_frame_std;
+						tx_frame.MsgID = MSG_MASTER_ID;
+						tx_frame.FIR.B.DLC = 2;
+						// tx_frame.data.u8[0] = device_id[i];
+						tx_frame.data.u8[1] = MSG_CONTROL_OPEN;
+						ESP32Can.CANWriteFrame(&tx_frame);
+					}
+					
+				}
+				else if(type.equals("mapping_name"))
+				{
+
+				}
+				else if(type.equals("set_name"))
+				{
+
+				}
+				else if(type.equals("set_all_device"))
+				{
+
+				}
+				else if(type.equals("setting"))
+				{
+
+				}
+			}
+			else
+			{
+				ECHOLN("wrong format!");
+			}
 		}
+	default:
+        break;
 	}
+	
 }
 
 
@@ -48,7 +118,7 @@ void setup() {
         ECHOLN("An error occurred initializing Bluetooth");
         // digitalWrite(ledTestWifi, LOW);
     } else {
-        ECHO("Bluetooth initialized: ");
+        ECHOLN("Bluetooth initialized: ");
         // ECHOLN(GetFullSSID().c_str());
     }
     SerialBT.register_callback(callback);
